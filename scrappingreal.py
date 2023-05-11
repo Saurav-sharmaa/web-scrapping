3ad7f7146afa53087b5c686c4dfd6724204ac87a
Saurav Tamta Sharma <sauravtamtasharma@gmail.com>
2023-05-11 05:40:43 +0530

    till save

--- scrappingreal.py
+++ scrappingreal.py
@@ -1,37 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation=').text
-# print(html_text)
+print("Enter skills you're not familiar with")
+unfam_skills = input(">")
+print("Filtering out jobs for you")
 
 
-soup = BeautifulSoup(html_text,'lxml')
- 
-# jobs = soup.find_all('li',class_='clearfix job-bx wht-shd-bx' )
+def findjobs():
+    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation=').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
 
-jobs = soup.find_all('li',class_='clearfix job-bx wht-shd-bx' )
+    for index, job in enumerate(jobs):
+        posdate = job.find('span', class_="sim-posted").span.text
 
-for job in jobs:
-    
-    posdate = job.find('span',class_="sim-posted").text
+        if 'few' in posdate:
 
-    if 'few' in posdate:
-    
-        company_name = job.find('h3', class_="joblist-comp-name").text.replace(" ", '')
-        skills = job.find('span',class_="srp-skills").text.replace(" ","")
-        moreinfo = job.header.h2.a['href']
+            company_name = job.find('h3', class_="joblist-comp-name").text.replace(" ", '')
+            skills = job.find('span', class_="srp-skills").text.replace(" ", "")
+            moreinfo = job.header.h2.a['href']
 
-        print(f"Company Name    : {company_name.strip()}")
-        print(f"Skills Required : {skills.strip()}")
-        print(f"More info       : {moreinfo}")
-        print(" ")
-
-    
+            if unfam_skills not in skills:
+                with open(f'files/{index}.txt','w') as f:
+                    f.write(f"Company Name    : {company_name.strip()}")
+                    f.write(f"Skills Required : {skills.strip()}")
+                    f.write(f"More info       : {moreinfo}")
 
 
 
-# print(f'''
-# Company Name    : {company_name}
-# Skills Required : {skills}
-# ''')
+
+
+
 
