c54feb857210e98cccdbe2f550ac5e7cdf0ba311
Saurav Tamta Sharma <sauravtamtasharma@gmail.com>
2023-05-11 05:51:21 +0530

    saving in file

--- scrappingreal.py
+++ scrappingreal.py
@@ -6,25 +6,23 @@ unfam_skills = input(">")
 print("Filtering out jobs for you")
 
 
-def findjobs():
-    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation=').text
-    soup = BeautifulSoup(html_text, 'lxml')
-    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation=').text
+soup = BeautifulSoup(html_text, 'lxml')
+jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
 
-    for index, job in enumerate(jobs):
-        posdate = job.find('span', class_="sim-posted").span.text
+for index, job in enumerate(jobs):
+     posdate = job.find('span', class_="sim-posted").span.text
 
-        if 'few' in posdate:
-
+     if 'few' in posdate:
             company_name = job.find('h3', class_="joblist-comp-name").text.replace(" ", '')
             skills = job.find('span', class_="srp-skills").text.replace(" ", "")
             moreinfo = job.header.h2.a['href']
 
             if unfam_skills not in skills:
-                with open(f'files/{index}.txt','w') as f:
-                    f.write(f"Company Name    : {company_name.strip()}")
-                    f.write(f"Skills Required : {skills.strip()}")
-                    f.write(f"More info       : {moreinfo}")
+                with open(f'files/{index}','w') as f:
+                  f.write(f"Company Name    : {company_name.strip()}")
+                  f.write(f"Skills Required : {skills.strip()}")
+                  f.write(f"More info       : {moreinfo}")
 
 
 
